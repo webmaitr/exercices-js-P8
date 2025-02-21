2353e0d15817e5ba7a71fbaf560764934fdc6e31
webmaitr <66726660+webmaitr@users.noreply.github.com>
2025-02-21 16:33:38 +0100

    Update index.js

--- Exercice1/index.js
+++ Exercice1/index.js
@@ -1,3 +1,11 @@
-//votre code ici
+function pairNumbers(a, b) {
+  for (let i=a; i<=b; i++) {
+    let st = [];
+    if (i%2 === 0) {
+      st.push(i);
+    }
+  }
+  return st.toString();
+}
 
 export default pairNumbers
